359c67ba53faf0c4ff1dd970619c2cbcc97b79fb
hiron <hiron@gmail.com>
2018-07-15 20:30:47 +0900

    リソースにアイコンデータを格納、モーメント図の出力に対応

--- BeamAnalysis/BeamAnalysis/BeamAnalysisComponent.cs
+++ BeamAnalysis/BeamAnalysis/BeamAnalysisComponent.cs
@@ -17,16 +17,11 @@ using Rhino.Geometry;
 
 namespace BeamAnalysis
 {
-    public class BeamAnalysisComponent : GH_Component
+    public class Beam_CL_Analysis : GH_Component
     {
         /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
         /// </summary>
-        public BeamAnalysisComponent()
+        public Beam_CL_Analysis()
           : base("Centralized Load",                 // 名称
                  "Centralized L",                    // 略称
                  "Stress Analysis of the Beam",      // コンポーネントの説明
@@ -45,7 +40,7 @@ namespace BeamAnalysis
         }
 
         /// <summary>
-        /// Registers all the input parameters for this component.
+        /// インプットパラメータの登録
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
@@ -56,7 +51,7 @@ namespace BeamAnalysis
         }
 
         /// <summary>
-        /// Registers all the output parameters for this component.
+        /// アウトプットパラメータの登録
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
@@ -68,15 +63,15 @@ namespace BeamAnalysis
         }
 
         /// <summary>
-        /// This is the method that actually does the work.
+        /// 計算部分
         /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
+        /// <param name="Param">断面性能計算コンポーネントからのデータを受け取るためのList</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // input
             // パラメータはひとまとめにするため、List にまとめる
             List<double> Param = new List<double>();
+            List<double> M_out = new List<double>();
             double P = double.NaN;
             double Lb = double.NaN;
             double E = double.NaN;
@@ -108,6 +103,13 @@ namespace BeamAnalysis
             Sig = M * 1000000 / Zy;
             D = P * 1000 * L * L * L / (48 * E * Iy);
 
+            M_out.Add(0);
+            M_out.Add(M / 2);
+            M_out.Add(M);
+            M_out.Add(M / 2);
+            M_out.Add(0);
+            M_out.Add(L);
+
             // 許容曲げの計算
             if (fb_calc == 0) // H強軸回りの場合
             {
@@ -130,7 +132,7 @@ namespace BeamAnalysis
             }
 
             // 出力設定
-            DA.SetData(0, M);
+            DA.SetDataList(0, M_out);
             DA.SetData(1, Sig);
             DA.SetData(2, fb);
             DA.SetData(3, Sig/fb);
@@ -138,15 +140,13 @@ namespace BeamAnalysis
         }
 
         /// <summary>
-        /// Provides an Icon for every component that will be visible in the User Interface.
-        /// Icons need to be 24x24 pixels.
+        /// アイコンの設定。24x24 pixelsが推奨
         /// </summary>
         protected override System.Drawing.Bitmap Icon
         {
             get
             {
-                Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\CL_icon.png");
-                return Resources;
+                return BeamAnalysis.Properties.Resource.CL_icon;
             }
         }
 
@@ -203,12 +203,12 @@ namespace BeamAnalysis
     }
 
 
-    public class Beam_UL_AnalysisComponent : GH_Component
+    public class Beam_TL_Analysis : GH_Component
     {
         /// <summary>
         /// 名称の設定
         /// </summary>
-        public Beam_UL_AnalysisComponent()
+        public Beam_TL_Analysis()
       : base("Trapezoid Load",      // 名称
              "Trapezoid L",                         // 略称
              "Stress Analysis of the Beam",      // コンポーネントの説明
@@ -252,6 +252,7 @@ namespace BeamAnalysis
         // input
         // パラメータはひとまとめにするため、List にまとめる
         List<double> Param = new List<double>();
+        List<double> M_out = new List<double>();
         double W = double.NaN;
         double DW = double.NaN;
         double Lb = double.NaN;
@@ -261,7 +262,7 @@ namespace BeamAnalysis
         double Sig = double.NaN;
         double D = double.NaN;
         //
-        double L, Iy, Zy, i_t, lamda, Af, F, H, fb_calc, fb, fb1, fb2;
+        double L, Iy, Zy, i_t, lamda, Af, F, H, fb_calc, fb, fb1, fb2, Ra, Mx;
         double C = 1.0;
 
         // Paramは List なので、GetDataList とする。
@@ -284,6 +285,15 @@ namespace BeamAnalysis
         M = (W/1000000) / 24 * (3 * L * L - 4 * DW * DW);
         Sig = M * 1000000 / Zy;
         D = (W/1000000) / (1920 * E * Iy) * (5 * L * L - 4 * DW * DW) * (5 * L * L - 4 * DW * DW);
+        Ra = (W / 1000000) * (L - DW) / 2; // 反力
+        Mx = (Ra * L / 4) - ((W / 1000000) * Math.Pow(L / 4, 3) / (6 * DW)); // 1/4点のモーメント計算
+
+        M_out.Add(0);
+        M_out.Add(Mx);
+        M_out.Add(M);
+        M_out.Add(Mx);
+        M_out.Add(0);
+        M_out.Add(L);
 
         fb1 = (1.0 - 0.4 * (Lb / i_t) * (Lb / i_t) / (C * lamda * lamda)) * F / 1.5;
         fb2 = 89000.0 / (Lb * H / Af);
@@ -311,7 +321,7 @@ namespace BeamAnalysis
         }
 
         // 出力設定
-        DA.SetData(0, M);
+        DA.SetDataList(0, M_out);
         DA.SetData(1, Sig);
         DA.SetData(2, fb);
         DA.SetData(3, Sig / fb);
@@ -326,8 +336,7 @@ namespace BeamAnalysis
     {
         get
         {
-            Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\UL_icon.png");
-            return Resources;
+            return BeamAnalysis.Properties.Resource.UL_icon;
         }
     }
 
@@ -456,8 +465,7 @@ namespace ModelDisp
         {
             get
             {
-                Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\H_icon.png");
-                return Resources;
+                return BeamAnalysis.Properties.Resource.H_icon;
             }
         }
 
@@ -566,8 +574,7 @@ namespace ModelDisp
         {
             get
             {
-                Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\L_icon.png");
-                return Resources;
+                return BeamAnalysis.Properties.Resource.L_icon;
             }
         }
 
@@ -679,16 +686,15 @@ namespace ModelDisp
             model[3] = Lweb;
 
             // まとめての出力なので、SetDataList で出力
-            DA.SetDataList(1, model);
             DA.SetDataList(0, Params);
+            DA.SetDataList(1, model);
         }
 
-        protected override System.Drawing.Bitmap Icon
+        protected override Bitmap Icon
         {
             get
             {
-                Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\BOX_icon.png");
-                return Resources;
+                return BeamAnalysis.Properties.Resource.BOX_icon;
             }
         }
 
@@ -704,12 +710,12 @@ namespace ModelDisp
 /// </summary>
 namespace ResultView
 {
-        public class ResultViewer : GH_Component
+        public class MomentViewer : GH_Component
     {
-        public ResultViewer()
-            : base("Make H Shape Model",
-                   "H Shape",
-                   "Display H Shape Model",
+        public MomentViewer()
+            : base("Moment View",
+                   "Moment",
+                   "Display Moment",
                    "rgkr",
                    "Result"
                   )
@@ -717,101 +723,73 @@ namespace ResultView
         }
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddNumberParameter("Width", "B", "Model Width (mm)", GH_ParamAccess.item, 200.0);
-            pManager.AddNumberParameter("Height", "H", "Model High (mm)", GH_ParamAccess.item, 400.0);
-            pManager.AddNumberParameter("Web Thickness", "tw", "Web Thickness (mm)", GH_ParamAccess.item, 8.0);
-            pManager.AddNumberParameter("Flange Thickness", "tf", "Flange Thickness (mm)", GH_ParamAccess.item, 13.0);
-            pManager.AddNumberParameter("F", "F", "F (N/mm2)", GH_ParamAccess.item, 235);
-            pManager.AddNumberParameter("Length", "L", "Model Length (mm)", GH_ParamAccess.item, 6300.0);
+            pManager.AddNumberParameter("Moment", "M", "Input Moment", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Scale", "Sc", "Input Output Scale", GH_ParamAccess.item, 1);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
-            pManager.AddNumberParameter("Analysis Parametar", "Param", "output Analysis Parameter", GH_ParamAccess.item);
-            pManager.AddSurfaceParameter("View Model Surface", "Srf", "output Model Surface", GH_ParamAccess.item);
+            pManager.AddBrepParameter("View Moment Surface", "Srf", "output Moment Surface", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             // 引数設定
-            double B = double.NaN;
-            double H = double.NaN;
-            double L = double.NaN;
-            double tw = double.NaN;
-            double tf = double.NaN;
-            double F = double.NaN;
-            double Iy, Zy, i_t, lamda, Af;
+            List<double> M = new List<double>();
+            double M1, M2, M3, M4, M5, L;
+            double Sc = double.NaN;
 
             // 入力設定
-            if (!DA.GetData(0, ref B)) { return; }
-            if (!DA.GetData(1, ref H)) { return; }
-            if (!DA.GetData(2, ref tw)) { return; }
-            if (!DA.GetData(3, ref tf)) { return; }
-            if (!DA.GetData(4, ref F)) { return; }
-            if (!DA.GetData(5, ref L)) { return; }
+            if (!DA.GetDataList(0, M)) { return; }
+            if (!DA.GetData(1, ref Sc)) { return; }
 
-            // 原点の作成
-            var Ori = new Point3d(0, 0, 0);
+            M1 = M[0];
+            M2 = M[1];
+            M3 = M[2];
+            M4 = M[3];
+            M5 = M[4];
+            L = M[5];
 
-            // 上フランジのサーフェス作成
-            var UFp1 = new Point3d(0, 0, H / 2);
-            var UFp2 = new Point3d(1, 0, H / 2);
-            var UFp3 = new Point3d(0, 1, H / 2);
-            var UFplane = new Plane(UFp1, UFp2, UFp3);
-            var upper_flange = new PlaneSurface(UFplane, new Interval(-B / 2, B / 2), new Interval(0, L));
+            // モーメント図の作成
+            var M12_P1 = new Point3d(0, 0, Sc * -M1);
+            var M12_P2 = new Point3d(0, L / 4, Sc * -M2);
+            var M12_P3 = new Point3d(0, L / 4, 0);
+            //
+            var M23_P1 = new Point3d(0, L / 4, 0);
+            var M23_P2 = new Point3d(0, L / 4, Sc * -M2);
+            var M23_P3 = new Point3d(0, L / 2, Sc * -M3);
+            var M23_P4 = new Point3d(0, L / 2, 0);
+            //
+            var M34_P1 = new Point3d(0, L / 2, 0);
+            var M34_P2 = new Point3d(0, L / 2, Sc * -M3);
+            var M34_P3 = new Point3d(0, 3 * L / 4, Sc * -M4);
+            var M34_P4 = new Point3d(0, 3 * L / 4, 0);
+            //
+            var M45_P1 = new Point3d(0, 3 * L / 4, 0);
+            var M45_P2 = new Point3d(0, 3 * L / 4, Sc * -M4);
+            var M45_P3 = new Point3d(0, L, Sc * -M5);
 
-            // 下フランジのサーフェス作成
-            var BFp1 = new Point3d(0, 0, -H / 2);
-            var BFp2 = new Point3d(1, 0, -H / 2);
-            var BFp3 = new Point3d(0, 1, -H / 2);
-            var BFplane = new Plane(BFp1, BFp2, BFp3);
-            var bottom_flange = new PlaneSurface(BFplane, new Interval(-B / 2, B / 2), new Interval(0, L));
-
-            // ウェブのサーフェス作成
-            var Wp1 = new Point3d(0, 0, 0);
-            var Wp2 = new Point3d(0, 0, -1);
-            var Wp3 = new Point3d(0, 1, 0);
-            var Wplane = new Plane(Wp1, Wp2, Wp3);
-            var web = new PlaneSurface(Wplane, new Interval(-H / 2, H / 2), new Interval(0, L));
-
-            // 解析用パラメータの計算
-            Iy = (1.0 / 12.0 * B * H * H * H) - (1.0 / 12.0 * (B - tw) * (H - 2 * tf) * (H - 2 * tf) * (H - 2 * tf));
-            Zy = Iy / (H / 2);
-
-            // 許容曲げ関連の計算
-            i_t = Math.Sqrt((tf * B * B * B + (H / 6.0 - tf) * tw * tw * tw) / (12 * (tf * B + (H / 6.0 - tf) * tw)));
-            lamda = 1500 / Math.Sqrt(F / 1.5);
-            Af = B * tf;
-
-            // ひとまとめにするため List で作成
-            List<double> Params = new List<double>();
-            Params.Add(L);  //  部材長さ
-            Params.Add(Iy); //  断面二次モーメント
-            Params.Add(Zy); //  断面係数
-            Params.Add(i_t);
-            Params.Add(lamda);
-            Params.Add(Af);
-            Params.Add(F);
-            Params.Add(H);
-
+            Brep M12_brep = Brep.CreateFromCornerPoints(M12_P1, M12_P2, M12_P3, GH_Component.DocumentTolerance());
+            Brep M23_brep = Brep.CreateFromCornerPoints(M23_P1, M23_P2, M23_P3, M23_P4, GH_Component.DocumentTolerance());
+            Brep M34_brep = Brep.CreateFromCornerPoints(M34_P1, M34_P2, M34_P3, M34_P4, GH_Component.DocumentTolerance());
+            Brep M45_brep = Brep.CreateFromCornerPoints(M45_P1, M45_P2, M45_P3, GH_Component.DocumentTolerance());
 
             // モデルはRhino上に出力するだけなので、とりあえず配列でまとめる
-            var model = new PlaneSurface[3];
-            model[0] = upper_flange;
-            model[1] = bottom_flange;
-            model[2] = web;
+            var brep = new Brep[4];
+            brep[0] = M12_brep;
+            brep[1] = M23_brep;
+            brep[2] = M34_brep;
+            brep[3] = M45_brep;
 
             // まとめての出力なので、SetDataList で出力
-            DA.SetDataList(1, model);
-            DA.SetDataList(0, Params);
+            DA.SetDataList(0, brep);
         }
 
         protected override System.Drawing.Bitmap Icon
         {
             get
             {
-                Bitmap Resources = new Bitmap(@"D:\Repos\BeamAnalysis\BeamAnalysis\BeamAnalysis\images\Result_M_icon.png");
-                return Resources;
+                return BeamAnalysis.Properties.Resource.Result_M_icon;
             }
         }
 
